33bfc80f3f7802fc8839e671b8149c40ab3a5349
Victor Lannes <victor.ferreiralannes@hotmail.com>
2023-01-17 14:15:59 -0300

    Adicionar arquivos de projeto.

---
+++ heranca/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using heranca.Entities;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            BusinessAccount account = new BusinessAccount();
+
+        }
+    }
+}
